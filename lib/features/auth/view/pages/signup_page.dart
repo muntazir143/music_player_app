dd93af62041d65082e270add2d8968fdde99c6e7
Muntazir Rizvi <smmr143@gmail.com>
2024-06-21 16:51:09 +0500

    auth pages

---
+++ lib/features/auth/view/pages/signup_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:music_player_app/core/theme/app_pallete.dart';
+import 'package:music_player_app/features/auth/view/widgets/auth_gradient_button.dart';
+import 'package:music_player_app/features/auth/view/widgets/custom_field.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Pallete.transparentColor,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sign Up.",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              CustomField(
+                hintText: "Name",
+                controller: nameController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomField(
+                hintText: "Email",
+                controller: emailController,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              CustomField(
+                hintText: "Password",
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              AuthGradientButton(
+                text: "Sign Up",
+                onPressed: () {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: "Already have an account? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: const [
+                    TextSpan(
+                      text: "Sign In",
+                      style: TextStyle(
+                        color: Pallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/auth/view/widgets/auth_gradient_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:music_player_app/core/theme/app_pallete.dart';
+
+class AuthGradientButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const AuthGradientButton(
+      {super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: [
+            Pallete.gradient1,
+            Pallete.gradient2,
+          ],
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(7),
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          fixedSize: const Size(395, 55),
+          backgroundColor: Pallete.transparentColor,
+          shadowColor: Pallete.transparentColor,
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
