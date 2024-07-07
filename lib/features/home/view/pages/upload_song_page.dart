116edf31e6583f983929360bebb7146943de985c
Muntazir Rizvi <smmr143@gmail.com>
2024-07-07 21:21:56 +0500

    upload song UI

--- lib/features/home/view/pages/upload_song_page.dart
+++ lib/features/home/view/pages/upload_song_page.dart
@@ -1,6 +1,8 @@
+import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:music_player_app/core/theme/app_pallete.dart';
+import 'package:music_player_app/core/widgets/custom_field.dart';
 
 class UploadSongPage extends ConsumerStatefulWidget {
   const UploadSongPage({super.key});
@@ -10,37 +12,78 @@ class UploadSongPage extends ConsumerStatefulWidget {
 }
 
 class _UploadSongPageState extends ConsumerState<UploadSongPage> {
+  final songNameController = TextEditingController();
+  final artistController = TextEditingController();
+  Color selectedColor = Pallete.cardColor;
+
+  @override
+  void dispose() {
+    songNameController.dispose();
+    artistController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Upload Song"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.cloud_upload_sharp),
+          ),
+        ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(25),
-              decoration: BoxDecoration(
-                border: Border.all(color: Pallete.borderColor),
-                borderRadius: BorderRadius.circular(10),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Pallete.borderColor),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Column(
+                  children: [
+                    Icon(
+                      Icons.folder_open,
+                      size: 40,
+                    ),
+                    SizedBox(height: 15),
+                    Text("Select the thumbnail for your song")
+                  ],
+                ),
               ),
-              child: const Column(
-                children: [
-                  Icon(
-                    Icons.folder_open,
-                    size: 40,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text("Select the thumbnail for your song")
-                ],
+              const SizedBox(height: 40),
+              CustomField(
+                hintText: "Pick Song",
+                controller: null,
+                readOnly: true,
+                onTap: () {},
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              CustomField(
+                hintText: "Song Name",
+                controller: songNameController,
+              ),
+              const SizedBox(height: 10),
+              CustomField(
+                hintText: "Artist",
+                controller: artistController,
+              ),
+              const SizedBox(height: 10),
+              ColorPicker(
+                pickersEnabled: const {
+                  ColorPickerType.wheel: true,
+                },
+                onColorChanged: (Color color) {},
+              ),
+            ],
+          ),
         ),
       ),
     );
