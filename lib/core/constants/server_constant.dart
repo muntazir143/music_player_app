355a42a666f43c180c8830a933879886d54429d3
Muntazir Rizvi <smmr143@gmail.com>
2024-07-24 14:38:01 +0100

    upload song image and audio picking

--- lib/core/constants/server_constant.dart
+++ lib/core/constants/server_constant.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 
 class ServerConstant {
   static String serverURL =
-      Platform.isAndroid ? "10.0.3:8000" : "http://127.0.0.1:8000";
+      Platform.isAndroid ? "http://10.0.2.2:8000" : "http://127.0.0.1:8000";
 }
--- lib/features/home/view/pages/upload_song_page.dart
+++ lib/features/home/view/pages/upload_song_page.dart
@@ -1,7 +1,10 @@
+import 'dart:io';
+
 import 'package:flex_color_picker/flex_color_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:music_player_app/core/theme/app_pallete.dart';
+import 'package:music_player_app/core/utils.dart';
 import 'package:music_player_app/core/widgets/custom_field.dart';
 
 class UploadSongPage extends ConsumerStatefulWidget {
@@ -15,6 +18,26 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
   final songNameController = TextEditingController();
   final artistController = TextEditingController();
   Color selectedColor = Pallete.cardColor;
+  File? selectedImage;
+  File? selectedAudio;
+
+  void selectAudio() async {
+    final pickedAudio = await pickAudio();
+    if (pickedAudio != null) {
+      setState(() {
+        selectedAudio = pickedAudio;
+      });
+    }
+  }
+
+  void selectImage() async {
+    final pickedImage = await pickImage();
+    if (pickedImage != null) {
+      setState(() {
+        selectedImage = pickedImage;
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -40,30 +63,47 @@ class _UploadSongPageState extends ConsumerState<UploadSongPage> {
           padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(25),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Pallete.borderColor),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: const Column(
-                  children: [
-                    Icon(
-                      Icons.folder_open,
-                      size: 40,
-                    ),
-                    SizedBox(height: 15),
-                    Text("Select the thumbnail for your song")
-                  ],
-                ),
+              GestureDetector(
+                onTap: selectImage,
+                child: selectedImage != null
+                    ? SizedBox(
+                        height: 150,
+                        width: double.infinity,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            selectedImage!,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      )
+                    : Container(
+                        height: 150,
+                        width: double.infinity,
+                        padding: const EdgeInsets.all(25),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Pallete.borderColor),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.folder_open,
+                              size: 40,
+                            ),
+                            SizedBox(height: 15),
+                            Text("Select the thumbnail for your song")
+                          ],
+                        ),
+                      ),
               ),
               const SizedBox(height: 40),
               CustomField(
                 hintText: "Pick Song",
                 controller: null,
                 readOnly: true,
-                onTap: () {},
+                onTap: selectAudio,
               ),
               const SizedBox(height: 10),
               CustomField(
